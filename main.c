8a191fd3ba89825c08835714e883bd0715e28a39
Aryan Garg <54898594+Aryan-Garg@users.noreply.github.com>
2022-06-08 12:06:09 +0530

    Complete Raw Mode with External Error Handling

--- main.c
+++ main.c
@@ -3,7 +3,9 @@
 #include <unistd.h>
 #include <termios.h>
 #include <ctype.h>
+#include <errno.h>
 
+#include "errorHandler.h"
 // STEPS: 
 // 1. Enter Raw Mode
 // 2. Handle Raw I/O
@@ -13,23 +15,37 @@
 struct termios orig_termios;
 
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) throwErrorMsg("tcsetattr");
 }
 
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
-  atexit(disableRawMode);
-  struct termios raw = orig_termios;
-  // Turn off:
-  raw.c_lflag &= ~(ECHO);
-  raw.c_lflag &= ~(ICANON); // canonical mode 
-  raw.c_lflag &= ~(ISIG); // ctrl-c, ctrl-z
-  raw.c_lflag &= ~(ICRNL); // carriage return -> see report
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) 
+		throwErrorMsg("tcgetattr");
+	atexit(disableRawMode);
+	struct termios raw = orig_termios;
+	// Turn off:
+	raw.c_lflag &= ~(ECHO);
+	raw.c_lflag &= ~(ICANON); // canonical mode 
+	raw.c_lflag &= ~(ISIG); // ctrl-c, ctrl-z
+	raw.c_lflag &= ~(ICRNL); // carriage return -> report
+	raw.c_oflag &= ~(OPOST); // output processing -> report
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) 
+		throwErrorMsg("tcsetattr");
 }
 
 int main(int argc, char *argv[]){
-	
+	enableRawMode();
+	while (1) {
+		char c = '\0';
+		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) 		throwErrorMsg("read not working in main loop");
+		
+		if (iscntrl(c)) printf("%d\r\n", c);
+		else printf("%d ('%c')\r\n", c, c);
+    
+		if (c == 'q') break;
+	}
+
 	
 	return 0;
 }
