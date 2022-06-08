599c702125385f4ea937508df30d8a728030a3a2
Aryan Garg <54898594+Aryan-Garg@users.noreply.github.com>
2022-06-08 12:17:22 +0530

    Update main.c

--- main.c
+++ main.c
@@ -6,33 +6,13 @@
 #include <errno.h>
 
 #include "errorHandler.h"
+#include "modeChanger.h"
+
 // STEPS: 
 // 1. Enter Raw Mode
 // 2. Handle Raw I/O
 // 3. Create Text Viewer Window/Screen
 // 4. Create the editor (main features)
-
-struct termios orig_termios;
-
-void disableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) throwErrorMsg("tcsetattr");
-}
-
-void enableRawMode() {
-	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) 
-		throwErrorMsg("tcgetattr");
-	atexit(disableRawMode);
-	struct termios raw = orig_termios;
-	// Turn off:
-	raw.c_lflag &= ~(ECHO);
-	raw.c_lflag &= ~(ICANON); // canonical mode 
-	raw.c_lflag &= ~(ISIG); // ctrl-c, ctrl-z
-	raw.c_lflag &= ~(ICRNL); // carriage return -> report
-	raw.c_oflag &= ~(OPOST); // output processing -> report
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) 
-		throwErrorMsg("tcsetattr");
-}
 
 int main(int argc, char *argv[]){
 	enableRawMode();
