1a2967fffc9d0d164d2971de3d9d89ba8843fce1
Aryan Garg <54898594+Aryan-Garg@users.noreply.github.com>
2022-06-11 10:12:47 +0530

    Update editorSpecs.c

--- editorSpecs.c
+++ editorSpecs.c
@@ -6,11 +6,13 @@
 #include <errno.h>
 #include <sys/ioctl.h>
 
+#include "cursorPosition.h"
+
 int findWinSz(int *rows, int *cols) {
   struct winsize window_size;
   int status = ioctl(STDOUT_FILENO, TIOCGWINSZ, &window_size);
   if(status == -1 || window_size.ws_col == 0){
-  		return -1;
+  		return findCursor(rows, cols);
   } 
   else{
         *cols = window_size.ws_col;
