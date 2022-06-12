e52ba582b5596daea4076602367b799f3708d866
Jainan-Tandel <56350648+Jainan-Tandel@users.noreply.github.com>
2022-06-12 06:34:37 +0530

    Added functions, but need to be included in main

--- keyProcessing.c
+++ keyProcessing.c
@@ -19,10 +19,30 @@ char keyRead() {
   return c;
 }
 
+
+void moveCursorEditor(char key) {
+  if(key=='a' && editor.curs_x > 0) {
+      editor.curs_x--;
+  }
+  else if(key=='d' && editor.curs_x < editor.number_of_cols - 1) {
+      editor.curs_x++;
+  }
+  else if(key=='w' && editor.curs_y > 0) {
+      editor.curs_y--;
+  }
+  else if(key=='s' && editor.curs_y < editor.number_of_rows - 1) {
+      editor.curs_y++;
+  }
+}
+if (E.cx != E.screencols - 1)
+
 void keypressEditor() {
   char c = keyRead();
   if(c==ctrl_key('q')){
   	  exit(0);
+  }
+  else if(c=='w' || c=='a' || c=='s' || c=='d'){
+	  moveCursorEditor(c);
   }
   
 }
